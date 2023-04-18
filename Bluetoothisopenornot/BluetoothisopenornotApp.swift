803a8ea13d413d85257889e7f8e426e349f334da
Ariful0007 <arifulpub143@gmail.com>
2023-04-18 11:08:31 +0600

    Initial Commit

---
+++ Bluetoothisopenornot/BluetoothisopenornotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothisopenornotApp.swift
+//  Bluetoothisopenornot
+//
+//  Created by sang on 27/9/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothisopenornotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bluetoothisopenornot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bluetoothisopenornot
+//
+//  Created by sang on 27/9/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
